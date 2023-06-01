3779ce16c66ff56b7f36e4d61e2b88ac9d37d0f7
ShanmukhaMadras <134970678+ShanmukhaMadras@users.noreply.github.com>
2023-05-31 23:20:33 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "smadras-aws-tf-state-bucket"
     key = "main"
-    region = "us-east-1"
+    region = "us-east-2"
     dynamodb_table = "smadras-dynamo-db-table"
   }
 }
